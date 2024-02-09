d9f4230cffdb8b9a17dc19aa6ed076eb09e7d94b
Ashish Kulkarni <ashishkulkarni19999@gmail.com>
2024-02-09 13:29:49 +0530

    unplugin router removed;

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter(),
     vue(),
   ],
   resolve: {
